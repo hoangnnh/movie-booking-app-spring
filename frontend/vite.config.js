95e9d9def6054a1a2e870882516e18447c4b4b69
hoangnnh <huyhoang.ngoc.nguyen@gmail.com>
2026-06-01 19:35:11 +0700

    chore: update gitignore, env example, app config and vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,19 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+    strictPort: true,
+    allowedHosts: [
+      'crib-countdown-uneasily.ngrok-free.dev'
+    ],
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+        secure: false
+      }
+    }
+  }
 });
